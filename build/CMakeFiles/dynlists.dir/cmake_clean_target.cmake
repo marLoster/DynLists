f7ff9e0e38351392c67a8c253354e051fad13465
Aiden <80407697+Aiden1123@users.noreply.github.com>
2021-10-26 20:59:31 +0200

    Delete build directory

--- build/CMakeFiles/dynlists.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libdynlists.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Admin/Documents/GitHub/DynList
-# Build directory: C:/Users/Admin/Documents/GitHub/DynList/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
